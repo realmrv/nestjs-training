4bcc1041ce75464b918d5d12b27b19a71d77730f
Roman Morgunov <intel.opus@gmail.com>
2021-06-01 03:40:08 +0300

    Review service created and service controller modified

--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,5 +1,6 @@
 import { TimeStamps, Base } from '@typegoose/typegoose/lib/defaultClasses';
 import { prop } from '@typegoose/typegoose';
+import { Types } from 'mongoose';
 
 export interface ReviewModel extends Base {}
 export class ReviewModel extends TimeStamps {
@@ -14,4 +15,7 @@ export class ReviewModel extends TimeStamps {
 
   @prop()
   rating: number;
+
+  @prop()
+  productId: Types.ObjectId;
 }
