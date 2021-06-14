c4b1b3ba0f569d29a9086a6c32b5f1e292ed3323
Roman Morgunov <intel.opus@gmail.com>
2021-06-14 15:05:34 +0300

    Added implementation of REST methods of the Product module, fixed bugs

---
+++ src/product/product.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from 'nestjs-typegoose';
+import { ProductModel } from './product.model';
+import { ModelType } from '@typegoose/typegoose/lib/types';
+import { ProductDto } from './dto/product.dto';
+import { FindProductDto } from './dto/find-product.dto';
+import { ReviewModel } from '../review/review.model';
+
+@Injectable()
+export class ProductService {
+  constructor(
+    @InjectModel(ProductModel)
+    private readonly productModel: ModelType<ProductModel>,
+  ) {}
+
+  async create(dto: ProductDto) {
+    return this.productModel.create(dto);
+  }
+
+  async findById(id: string) {
+    return this.productModel.findById(id).exec();
+  }
+
+  async deleteById(id: string) {
+    return this.productModel.findByIdAndDelete(id).exec();
+  }
+
+  async updateById(id: string, dto: ProductDto) {
+    return this.productModel.findByIdAndUpdate(id, dto, { new: true }).exec();
+  }
+
+  async findWithReview(dto: FindProductDto) {
+    return this.productModel
+      .aggregate([
+        {
+          $match: {
+            categories: dto.category,
+          },
+        },
+        {
+          $sort: {
+            _id: 1,
+          },
+        },
+        {
+          $limit: dto.limit,
+        },
+        {
+          $lookup: {
+            from: 'Review',
+            localField: '_id',
+            foreignField: 'productId',
+            as: 'reviews',
+          },
+        },
+        {
+          $addFields: {
+            reviewCount: { $size: '$reviews' },
+            reviewAvg: { $avg: '$reviews.rating' },
+          },
+        },
+      ])
+      .exec() as (ProductModel & {
+      reviews: ReviewModel[];
+      reviewCount: number;
+      reviewAvg: number;
+    })[];
+  }
+}
