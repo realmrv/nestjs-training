3f74cf79b0d128e9265a9c8f0e49cf575f7c5cf8
Roman Morgunov <intel.opus@gmail.com>
2021-06-14 11:14:31 +0300

    Added registration and authorization via JWT

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -7,23 +7,27 @@ import {
   HttpStatus,
   Param,
   Post,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewService } from './review.service';
 import { REVIEW_NOT_FOUND } from './review.constants';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 
 @Controller('review')
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post('create')
   @UsePipes(new ValidationPipe())
   async create(@Body() dto: CreateReviewDto) {
     return this.reviewService.create(dto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   async delete(@Param('id') id: string) {
     const deletedDoc = await this.reviewService.delete(id);
