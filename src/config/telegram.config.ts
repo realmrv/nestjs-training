2c5f42008cf9e75e809bf65ec5d7abc029013bed
realmrv <intel.opus@gmail.com>
2021-06-17 15:35:32 +0300

    Added module TelegramModule to notify about adding comments

---
+++ src/config/telegram.config.ts
@@ -0,0 +1,16 @@
+import { ITelegramOptions } from '../telegram/telegram.interface';
+import { ConfigService } from '@nestjs/config';
+
+export const getTelegramConfig = (
+  configService: ConfigService,
+): ITelegramOptions => {
+  const token = configService.get('TELEGRAM_TOKEN');
+  if (!token) {
+    throw new Error('TELEGRAM_TOKEN не задан');
+  }
+
+  return {
+    token,
+    chatId: configService.get('CHAT_ID') ?? '',
+  };
+};
--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -16,16 +16,34 @@ import { ReviewService } from './review.service';
 import { REVIEW_NOT_FOUND } from './review.constants';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 import { IdValidationPipe } from '../pipes/id-validation.pipe';
+import { TelegramService } from '../telegram/telegram.service';
 
 @Controller('review')
 export class ReviewController {
-  constructor(private readonly reviewService: ReviewService) {}
+  constructor(
+    private readonly reviewService: ReviewService,
+    private readonly telegramService: TelegramService,
+  ) {}
 
   @UseGuards(JwtAuthGuard)
   @Post('create')
   @UsePipes(new ValidationPipe())
   async create(@Body() dto: CreateReviewDto) {
     return this.reviewService.create(dto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('notify')
+  @UsePipes(new ValidationPipe())
+  async notify(@Body() dto: CreateReviewDto) {
+    const message =
+      `Имя: ${dto.name}\n` +
+      `Заголовок: ${dto.title}\n` +
+      `Описание: ${dto.description}\n` +
+      `Рейтинг: ${dto.rating}\n` +
+      `ID Продукта: ${dto.title}`;
+
+    return this.telegramService.sendMessage(message);
   }
 
   @UseGuards(JwtAuthGuard)
